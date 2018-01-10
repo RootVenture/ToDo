41361932f7b80ff03d0671c5f48dd01b05b7b297
Rootventure <raywupharmd@gmail.com>
2018-01-09 23:07:03 -0500

    Basic HTML page and updated scripts.js to interact with html

--- scripts.js
+++ scripts.js
@@ -1,19 +1,5 @@
 const todoList = {
   todos: [],
-  display() {
-    if (this.todos.length === 0) {
-      console.log('Your todo list is empty!');
-      return;
-    }
-    console.log('My Todos');
-    for (let i = 0; i < this.todos.length; i++) {
-      if (this.todos[i].done) {
-        console.log('(x)', this.todos[i].item);
-      } else {
-        console.log('()', this.todos[i].item);
-      }
-    }
-  },
   add(item) {
     this.todos.push({
       item,
@@ -22,21 +8,96 @@ const todoList = {
   },
   update(i, newItem) {
     this.todos[i].item = newItem;
-    this.display();
   },
   remove(i) {
     this.todos.splice(i, 1);
-    this.display();
   },
   toggleOne(i) {
     this.todos[i].done = !this.todos.done;
-    this.display();
   },
   toggleAll() {
+    let count = 0;
+    // get number of completed tasks
     for (let i = 0; i < this.todos.length; i++) {
-      // if everything is true, make everything false
-      
-      // else, everything true
+      if (this.todos[i].done === true) {
+        count++;
+      }
     }
-  }
+    // if every task is done, clear done on all tasks
+    if (count === this.todos.length) {
+      for (let i = 0; i < this.todos.length; i++) {
+        this.todos[i].done = false;
+      }
+    } else {
+      for (let i = 0; i < this.todos.length; i++) {
+        this.todos[i].done = true;
+      }
+    }
+  },
 };
+
+// HTML elements to select
+const toggleAll = document.getElementById('toggleAll');
+const addTodo = document.getElementById('addTodo');
+const todoUpdate = document.getElementById('todoUpdate');
+const deleteTodo = document.getElementById('deleteTodo');
+const toggleTodo = document.getElementById('toggleTodo');
+const view = {
+  display() {
+    const todoUl = document.querySelector('#todo');
+    todoUl.innerHTML = '';
+
+    todoList.todos.forEach(todo => {
+      const todoLi = document.createElement('li');
+      let content = '';
+
+      if (todo.done === true) {
+        content = `(x) ${todo.item}`;
+      } else {
+        content = `() ${todo.item}`;
+      }
+      todoLi.textContent = content;
+      todoUl.appendChild(todoLi);
+    });
+  },
+};
+
+// All event listeners
+addTodo.addEventListener('click', () => {
+  const addTodoInput = document.getElementById('addTodoInput');
+
+  todoList.add(addTodoInput.value);
+  addTodoInput.value = '';
+  view.display();
+});
+
+todoUpdate.addEventListener('click', () => {
+  const positionUpdate = document.getElementById('positionUpdate');
+  const textUpdate = document.getElementById('textUpdate');
+
+  todoList.update(positionUpdate.valueAsNumber, textUpdate.value);
+  positionUpdate.value = '';
+  textUpdate.value = '';
+  view.display();
+});
+
+deleteTodo.addEventListener('click', () => {
+  const deleteInput = document.getElementById('deleteInput');
+
+  todoList.remove(deleteInput.valueAsNumber);
+  deleteInput.value = '';
+  view.display();
+});
+
+toggleTodo.addEventListener('click', () => {
+  const toggleInput = document.getElementById('toggleInput');
+
+  todoList.toggleOne(toggleInput.valueAsNumber);
+  toggleTodo.value = '';
+  view.display();
+});
+
+toggleAll.addEventListener('click', () => {
+  todoList.toggleAll();
+  view.display();
+});
